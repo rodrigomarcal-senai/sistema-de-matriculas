b4d33b87c7079fa29e4757f13f80ca860d8be4eb
Rodrigo <ti5.contratosc@gmail.com>
2024-12-12 15:59:13 -0300

    Adicionar namespaces ao app usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "usuarios"
 urlpatterns = [
     path("cadastro/", views.cadastrar_usuario, name="cadastro"),
     path("login/", views.login_usuario, name="login"),
